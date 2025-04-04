1017025235c3ad7a9fb5861af15841af4ce3c73a
Curtis Cole <rcurtiscole@gmail.com>
2025-04-04 10:47:44 -0700

    looking for defn BinarySensor

--- components/pentair_component/pentair_component.h
+++ components/pentair_component/pentair_component.h
@@ -11,16 +11,16 @@ namespace pentair_component {
 
 class PentairRS422 : public uart::UARTDevice, public Component {
   protected:
-  sensor::BinarySensor *spa_on_sensor_{nullptr};
-  sensor::BinarySensor *aux1_sensor_{nullptr};
-  sensor::BinarySensor *aux2_sensor_{nullptr};
-  sensor::BinarySensor *aux3_sensor_{nullptr};
-  sensor::BinarySensor *pool_on_sensor_{nullptr};
-  sensor::BinarySensor *feature1_sensor_{nullptr};
-  sensor::BinarySensor *feature2_sensor_{nullptr};
-  sensor::BinarySensor *feature3_sensor_{nullptr};
-  sensor::BinarySensor *feature4_sensor_{nullptr};
-  sensor::BinarySensor *heater_on_sensor_{nullptr};
+  binary_sensor::BinarySensor *spa_on_sensor_{nullptr};
+  binary_sensor::BinarySensor *aux1_sensor_{nullptr};
+  binary_sensor::BinarySensor *aux2_sensor_{nullptr};
+  binary_sensor::BinarySensor *aux3_sensor_{nullptr};
+  binary_sensor::BinarySensor *pool_on_sensor_{nullptr};
+  binary_sensor::BinarySensor *feature1_sensor_{nullptr};
+  binary_sensor::BinarySensor *feature2_sensor_{nullptr};
+  binary_sensor::BinarySensor *feature3_sensor_{nullptr};
+  binary_sensor::BinarySensor *feature4_sensor_{nullptr};
+  binary_sensor::BinarySensor *heater_on_sensor_{nullptr};
   button::Button  *spa_button_{nullptr};
   sensor::Sensor *water_temperature_sensor_{nullptr};
   sensor::Sensor *air_temperature_sensor_{nullptr};
@@ -30,7 +30,7 @@ class PentairRS422 : public uart::UARTDevice, public Component {
  public:
   void setup() override;
 
-  void set_spa_on_sensor(sensor::Sensor *spa_on_sensor) { spa_on_sensor_ = spa_on_sensor; }
+  void set_spa_on_sensor(binary_sensor::BinarySensor *spa_on_sensor) { spa_on_sensor_ = spa_on_sensor; }
   void set_spa_button(button::Button *spa_button) { spa_button_ = spa_button; }
   void set_water_temperature_sensor(sensor::Sensor *temperature_sensor) { water_temperature_sensor_ = temperature_sensor; }
 
